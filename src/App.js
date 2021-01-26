09c815e961abf39289d7021bb7996f10d5ba51f4
KevinAbreuPortfolio <KevinAbreuGithub@hotmail.com>
2021-01-26 16:09:01 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState,useEffect} from 'react'; 
+import {Route,Switch} from 'react-router-dom';
+import Axios from 'axios';
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 function App() {
+  const [species,setSpecies] = useState();
+  const [hp,setHp] = useState();
+  const [atk,setAtk] = useState();
+  const [def,setDef] = useState();
+  const [spatk,setSpatk] = useState();
+  const [spdef,setSpdef] = useState();
+  const [speed,setSpeed] = useState();
+  const [image,setImage] = useState();
+
+  const getPokemon = () =>{
+    let dexNum = Math.floor(Math.random() * 899);
+    const zeroPad = (num, places) => String(num).padStart(places, '0')
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${dexNum}`).then((response)=>{
+      // console.log(response);
+      // setSpecies(response.data.name);
+      setSpecies(capitalizeFirstLetter(response.data.name));
+      setHp(response.data.stats[0]["base_stat"]);
+      setAtk(response.data.stats[1]["base_stat"]);
+      setDef(response.data.stats[2]["base_stat"]);
+      setSpatk(response.data.stats[3]["base_stat"]);
+      setSpdef(response.data.stats[4]["base_stat"]);
+      setSpeed(response.data.stats[5]["base_stat"]);
+      setImage(`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${zeroPad(dexNum,3)}.png`)
+    })
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Testing</h1> <button onClick={getPokemon}>Im feeling lucky</button>
+      <h1>{species}</h1>
+      <h1>Hp:{hp}</h1>
+      <h1>Attack{atk}</h1>
+      <h1>Defense:{def}</h1>
+      <h1>Special Attack:{spatk}</h1>
+      <h1>Special Defense:{spdef}</h1>
+      <h1>Speed:{speed}</h1>
+      <img src={image}></img>
+
+    </>
   );
 }
 
